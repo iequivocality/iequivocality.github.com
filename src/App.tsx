3ee0b1d97b684838dd04593f86620acfa27a41c4
iequivocality <xanthiandere@gmail.com>
2020-01-20 23:12:13 +0800

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { IoLogoTwitter, IoLogoGithub } from 'react-icons/io';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      IEQUIVOCALITY HERE
+      <h2>アンビデレです。私のページへようこそ！</h2>
+      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ambidere">
+        <IoLogoTwitter size={25} color={'#E53D77'}></IoLogoTwitter>
+      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/iequivocality">
+        <IoLogoGithub size={25} color={'#E53D77'}></IoLogoGithub>
+      </a>
     </div>
   );
 }
