105c41f5d5367798ae22632d12897e56081955e3
iequivocality <xanthiandere@gmail.com>
2020-01-20 22:23:27 +0800

    Updates

---
+++ precache-manifest.c888db3ee3e47bda4305d47446b5fef7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e52e5c509ec60c4b47e8ca34dd7fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fc4e7a8f8f0b825f47a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1754c13ed87e420a9053",
+    "url": "/static/js/2.277223a6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.277223a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fc4e7a8f8f0b825f47a",
+    "url": "/static/js/main.a514b60c.chunk.js"
+  },
+  {
+    "revision": "be3619793fb86b0d09be",
+    "url": "/static/js/runtime-main.a35d4c63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
