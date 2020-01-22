23ac3068107aa47ae5dbd36e36720207ab482421
iequivocality <xanthiandere@gmail.com>
2020-01-23 00:02:23 +0800

    cleaned and starting to add details

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ import { IoLogoTwitter, IoLogoGithub } from 'react-icons/io';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h2>アンビデレです。私のページへようこそ！</h2>
-      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ambidere">
-        <IoLogoTwitter size={25} color={'#E53D77'}></IoLogoTwitter>
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/iequivocality">
-        <IoLogoGithub size={25} color={'#E53D77'}></IoLogoGithub>
-      </a>
+      <div className="Page">
+        <div className="Page-Title">ambi</div>
+        <div className="Page-Subtitle">software developer</div>
+        <div className="Page-Subtitle">aidoru fan</div>
+        <div className="Page-Buttonbar">
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ambidere">
+            <IoLogoTwitter size={25} color={'#E53D77'}></IoLogoTwitter>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/iequivocality">
+            <IoLogoGithub size={25} color={'#E53D77'}></IoLogoGithub>
+          </a>
+        </div>
+      </div>
+      {/* <h2>アンビデレです。私のページへようこそ！</h2> */}
     </div>
   );
 }
